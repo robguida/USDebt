ab6d19c3eb69de0e4bc34de044b0d23cf61f2279
guidamedia <robguida4@gmail.com>
2017-06-10 00:16:28 -0400

    Adding in presidential information

--- index.php.php
+++ index.php.php
@@ -25,6 +25,41 @@ foreach ($css_files as $file) {
     $head .= "<link rel=\"stylesheet\" href=\"{$file}\" />\n";
 }
 
+/* presidential array so we can link the candidate to the debt */
+$pres_array = array (
+    727506000 => array(
+        'pres' => 'Bill Clinton',
+        'start' => 'January 20, 1993',
+        'end' => 'January 20, 2001',
+        'img' => 'Bill_Clinton.jpg'
+    ),
+    979966800 => array(
+        'pres' => 'George W. Bush',
+        'start' => 'January 20, 2001',
+        'end' => 'January 20, 2009',
+        'img' => 'George-W-Bush.jpg'
+    ),
+    1232427600 => array(
+        'pres' => 'Barack Obama',
+        'start' => 'January 20, 2009',
+        'end' => 'January 20, 2017',
+        'img' => 'Barack_Obama.jpg'
+    ),
+    1484888400 => array(
+        'pres' => 'Donald Trump',
+        'start' => 'January 20, 2017',
+        'end' => '',
+        'img' => 'Donald_Trump.jpg'
+    ),
+);
+$pres_nav = '';
+foreach ($pres_array as $pres) {
+    $pres_start = new DateTime($pres['start']);
+    $pres_end = new DateTime($pres['end']);
+    $pres_nav .= "<a href=\"?start_date={$pres_start->format('Y-m-d')}&end_date={$pres_end->format('Y-m-d')}\">" .
+                    "<img src=\"images/{$pres['img']}\" title=\"{$pres['pres']}\"></a>";
+}
+
 /* get the chart data */
 $startDt = new DateTime();
 $start_date = isset($_GET['start_date']) ? $_GET['start_date'] : $startDt->modify('-1 month')->format('Y-m-d');
@@ -33,6 +68,7 @@ $dot_url = "https://www.treasurydirect.gov/NP_WS/debt/search?startdate={$start_d
 
 /* if we have data, build the output */
 if ($data = current(json_decode(file_get_contents($dot_url)))) {
+    /* start the tabs and contents for each */
     $output = '<div id="tabs">' .
                     '<ul>' .
                     '<li><a href="#t1">Graph</a></li>' .
@@ -44,13 +80,31 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
     $graph_labels_arr = array();
     $graph_values = '[]';
     $graph_values_arr = array();
+    $graph_colors = '[]';
+    $graph_colors_arr = array();
+    $graph_colors_toggle = false;
     foreach ($data as $d) {
         $dateDt = new DateTime($d->effectiveDate);
         $debt_amount = '$' . number_format($d->totalDebt, 2);
+
+        /* add a record to the data table */
         $output .= "<tr><td class=\"date\">{$dateDt->format('m/d/Y')}</td>" .
                     "<td class=\"currency\">{$debt_amount}</td></tr>";
+
+        /* is this a day when a president took office? if so switch the color. */
+        if (array_key_exists($dateDt->getTimestamp(), $pres_array)) {
+            $graph_colors_toggle = !$graph_colors_toggle;
+            $graph_colors_arr[] = 'rgba(54, 162, 235, 0.2)';
+        } elseif (!$graph_colors_toggle) {
+            $graph_colors_arr[] = 'rgba(255, 99, 132, 0.2)';
+        } else {
+            /* get the last color and add it to the array */
+            $graph_colors_arr[] = end($graph_colors_arr);
+        }
+
         /* use the date for the labels on the graph */
         $graph_labels_arr[] = $dateDt->format('n/j/y');
+
         /* use the debt for the points on the graph */
         $graph_values_arr[] = round($d->totalDebt/1000000000000, 2);
     }
@@ -59,6 +113,9 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
     }
     if (!empty($graph_values_arr)) {
         $graph_values = '["' . implode('", "', $graph_values_arr) . '"]';
+    }
+    if (!empty($graph_colors_arr)) {
+        $graph_colors = '["' . implode('", "', $graph_colors_arr) . '"]';
     }
     $output .= '</tbody><tfoot></tfoot></table></div></div>';
 } else {
@@ -73,6 +130,18 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
     <style>
         body {
             padding: 16px;
+        }
+        div.pres_nav {
+            display: inline;
+        }
+        div.pres_nav img{
+            width: 50px;
+            height: 60px;
+            border: 1px solid black;
+            margin: 0px 10px 0px 0px;
+        }
+        form.search {
+            display: inline;
         }
         .chart-container {
             position: relative;
@@ -104,13 +173,14 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
 </head>
 <body>
 <h1>Our U.S. Debt</h1>
-<form id="search">
+<form id="search" class="search">
     <label for="start_date">Start Date:</label>
     <input type="text" id="start_date" name="start_date" value="<?php echo $start_date; ?>" />
     <label for="end_date">End Date:</label>
     <input type="text" id="end_date" name="end_date" value="<?php echo $end_date; ?>" />
     <input type="submit" id="submit" name="submit" value="Fetch" />
 </form>
+<div class="pres_nav"><?php echo $pres_nav; ?></div>
 <?php echo $output; ?>
 </body>
 <footer>
@@ -127,6 +197,7 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
             changeMonth: true,
             changeYear: true,
             dateFormat: 'yy-mm-dd',
+            minDate: new Date(1993, 0, 4),
         });
         $('#end_date').datepicker({
             showOtherMonths: true,
@@ -134,6 +205,7 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
             changeMonth: true,
             changeYear: true,
             dateFormat: 'yy-mm-dd',
+            minDate: new Date(1993, 1, 4),
         });
         var ctx = $("#myChart");
         var myChart = new Chart(ctx, {
@@ -143,22 +215,8 @@ if ($data = current(json_decode(file_get_contents($dot_url)))) {
                 datasets: [{
                     label: 'Our U.S. Debt (trillions)',
                     data: <?php echo $graph_values; ?>,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-//                        'rgba(54, 162, 235, 0.2)',
-//                        'rgba(255, 206, 86, 0.2)',
-//                        'rgba(75, 192, 192, 0.2)',
-//                        'rgba(153, 102, 255, 0.2)',
-//                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-//                        'rgba(54, 162, 235, 1)',
-//                        'rgba(255, 206, 86, 1)',
-//                        'rgba(75, 192, 192, 1)',
-//                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
+                    backgroundColor: <?php echo $graph_colors; ?>,
+                    borderColor: ['rgba(255,99,132,1)'],
                     borderWidth: 1
                 }]
             },
