3114ff9107d49cd5f86a8087f74569f693d4570d
robert <RGu!d@68>
2018-06-09 14:19:42 -0400

    Moved processing Stats tab data to the view file.

--- src/lib/Controller/USDebtController.php
+++ src/lib/Controller/USDebtController.php
@@ -64,78 +64,9 @@ class USDebtController
                 'tab_graph_Chart.php',
                 ['datas' => $datas, 'pres_array' => PresidentService::getPresidentConfig()]
             ),
-            'stats' => $this->getStatsData($datas),
+            'stats' => $output = $this->loadFile('tab_stats.php', ['datas' => $datas]),
         ];
         return $this->loadFile('tabs.php', $tab_sections);
-    }
-
-    private function getStatsData(array $datas)
-    {
-        /* we need the first and last debt to get the delta,
-            current($data)and to compute the debt per day for the date range */
-        /* we need the first and last debt to get the delta, and to compute the debt per day for the date range */
-        $lastDebt = current($datas);
-        $firstDebt = end($datas);
-        $firstDate = new DateTime($lastDebt->effectiveDate);
-        $lastDate = new DateTime($firstDebt->effectiveDate);
-        $working_days = count($datas);
-        $days = $firstDate->diff($lastDate)->days;
-        $delta = $lastDebt->totalDebt - $firstDebt->totalDebt;
-        $average_per_day = round($delta / $working_days, 2);
-        if (0 > $delta) {
-            $delta_str = '-$' . number_format(abs($delta), 2) . '';
-        } else {
-            $delta_str = '$' . number_format($delta, 2);
-        }
-        if (0 > $average_per_day) {
-            $average_per_day_str = '-$' . number_format(abs($average_per_day), 2) . '';
-        } else {
-            $average_per_day_str = '$' . number_format($average_per_day, 2);
-        }
-        $days_lt_start_debt = [];
-        $days_gt_start_debt = [];
-        $average_per_time_span = 0;
-        foreach ($datas as $d) {
-            /* if the amount is greather than the starting date amount, then add it to the array of days */
-            if ($firstDebt->totalDebt < $d->totalDebt) {
-                $days_gt_start_debt[] = $d;
-            }
-            /* if the amount is less than the starting date amount, then add it to the array of days */
-            if ($firstDebt->totalDebt > $d->totalDebt) {
-                $days_lt_start_debt[] = $d;
-            }
-            /* enter the debt values into an array to get the average */
-            $average_per_time_span += $d->totalDebt;
-        }
-
-        /* calculate greater than debt */
-        $days_gt_start_debt_count = count($days_gt_start_debt);
-        $days_gt_start_debt_percentage = round(($days_gt_start_debt_count / $working_days) * 100, 2);
-
-        /* calculate greater than debt */
-        $days_lt_start_debt_count = count($days_lt_start_debt);
-        $days_lt_start_debt_percentage = round(($days_lt_start_debt_count / $working_days) * 100, 2);
-
-        /* calucate the average for the time span */
-        $average_per_time_span /= $working_days;
-
-        $view_data = [
-            'last_debt' => $lastDebt,
-            'first_debt' => $firstDebt,
-            'firstDate' => new DateTime($lastDebt->effectiveDate),
-            'lastDate' => new DateTime($firstDebt->effectiveDate),
-            'working_days' => $working_days,
-            'average_per_time_span' => $average_per_time_span,
-            'days_gt_start_debt_count' => $days_gt_start_debt_count,
-            'days_gt_start_debt_percentage' => $days_gt_start_debt_percentage,
-            'days_lt_start_debt_count' => $days_lt_start_debt_count,
-            'days_lt_start_debt_percentage' => $days_lt_start_debt_percentage,
-            'delta_str' => $delta_str,
-            'average_per_day_str' => $average_per_day_str,
-            'days' => $days,
-        ];
-        $output = $this->loadFile('tab_stats.php', $view_data);
-        return $output;
     }
 
     /**
--- src/lib/View/tab_stats.php
+++ src/lib/View/tab_stats.php
@@ -5,11 +5,61 @@
  * Date: 6/5/18
  * Time: 9:43 PM
  */
+/* we need the first and last debt to get the delta,
+            current($data)and to compute the debt per day for the date range */
+/* we need the first and last debt to get the delta, and to compute the debt per day for the date range */
+$lastDebt = current($datas);
+$firstDebt = end($datas);
+$firstDate = new DateTime($lastDebt->effectiveDate);
+$lastDate = new DateTime($firstDebt->effectiveDate);
+$working_days = count($datas);
+$days = $firstDate->diff($lastDate)->days;
+$delta = $lastDebt->totalDebt - $firstDebt->totalDebt;
+$average_per_day = round($delta / $working_days, 2);
+if (0 > $delta) {
+    $delta_str = '-$' . number_format(abs($delta), 2) . '';
+} else {
+    $delta_str = '$' . number_format($delta, 2);
+}
+if (0 > $average_per_day) {
+    $average_per_day_str = '-$' . number_format(abs($average_per_day), 2) . '';
+} else {
+    $average_per_day_str = '$' . number_format($average_per_day, 2);
+}
+$days_lt_start_debt = [];
+$days_gt_start_debt = [];
+$average_per_time_span = 0;
+foreach ($datas as $d) {
+    /* if the amount is greather than the starting date amount, then add it to the array of days */
+    if ($firstDebt->totalDebt < $d->totalDebt) {
+        $days_gt_start_debt[] = $d;
+    }
+    /* if the amount is less than the starting date amount, then add it to the array of days */
+    if ($firstDebt->totalDebt > $d->totalDebt) {
+        $days_lt_start_debt[] = $d;
+    }
+    /* enter the debt values into an array to get the average */
+    $average_per_time_span += $d->totalDebt;
+}
+
+/* calculate greater than debt */
+$days_gt_start_debt_count = count($days_gt_start_debt);
+$days_gt_start_debt_percentage = round(($days_gt_start_debt_count / $working_days) * 100, 2);
+
+/* calculate greater than debt */
+$days_lt_start_debt_count = count($days_lt_start_debt);
+$days_lt_start_debt_percentage = round(($days_lt_start_debt_count / $working_days) * 100, 2);
+
+/* calucate the average for the time span */
+$average_per_time_span /= $working_days;
+$firstDate = new DateTime($lastDebt->effectiveDate);
+$lastDate = new DateTime($firstDebt->effectiveDate);
+
 ?>
 <h3>Starting debt amount</h3>
-<p>$<?php echo number_format($first_debt->totalDebt, 2); ?></p>
+<p>$<?php echo number_format($firstDebt->totalDebt, 2); ?></p>
 <h3>Last debt amount of date range</h3>
-<p>$<?php echo number_format($last_debt->totalDebt, 2); ?></p>
+<p>$<?php echo number_format($lastDebt->totalDebt, 2); ?></p>
 <h3>During the given time span, the debt increased</h3>
 <p><?php echo $delta_str; ?></p>
 <h3>This works out to be</h3>
